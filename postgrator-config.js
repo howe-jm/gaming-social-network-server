16156c855f436e46c4ebccb5b99a635027069db0
Dominic Faulring <domcodesjs@gmail.com>
2020-12-15 10:23:07 -0800

    added an additional ternary statement in postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,8 +1,17 @@
-const { NODE_ENV, TEST_DATABASE_URL, LOCAL_DATABASE_URL } = require('./config');
+const {
+  NODE_ENV,
+  TEST_DATABASE_URL,
+  LOCAL_DATABASE_URL,
+  DATABASE_URL
+} = require('./config');
 
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
-  // switch the last part to DATABASE_URL before launching to production
-  connectionString: NODE_ENV === 'test' ? TEST_DATABASE_URL : LOCAL_DATABASE_URL
+  connectionString:
+    NODE_ENV === 'test'
+      ? TEST_DATABASE_URL
+      : NODE_ENV === 'development'
+      ? LOCAL_DATABASE_URL
+      : DATABASE_URL
 };
