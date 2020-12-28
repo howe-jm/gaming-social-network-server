eb3a3ec55f71c7cb1c100c8575881460da8f3eb3
Dominic Faulring <domcodesjs@gmail.com>
2020-12-28 11:36:12 -0800

    templated out createUser route

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,5 @@
 // No need to module.exports in this file. When using exports it'll automatically be attached to the module.exports object. exports is perfect when you expect to create many different functions that are going to be exported.
 
-exports.createUser = async () => {
+exports.createUser = async (req, res) => {
   // code here
 };
