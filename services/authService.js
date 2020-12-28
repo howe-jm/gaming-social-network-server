83be60dbf7923d7c9e5f07829b2c731e28aad561
Dominic Faulring <domcodesjs@gmail.com>
2020-12-28 08:07:11 -0800

    day 1

---
+++ services/authService.js
@@ -0,0 +1,12 @@
+const db = require('../knex/knex');
+
+exports.addUser = async (req, res) => {
+  try {
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      errors: [{ msg: '' }]
+    });
+  }
+};
--- services/registrationService.js
+++
@@ -1,5 +0,0 @@
-// thinking of a good name for this file. Maybe authService? Since we may have to delete users later.
-
-exports.addUser = async () => {
-  // code here
-};
