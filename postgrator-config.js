efb8e4adcd8a8456b7ebfc660779bf774ba22a4e
Jason Howe <trisynth@gmail.com>
2020-12-28 12:53:36 -0500

    usersService endpoints added.

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,17 +1,8 @@
-const {
-  NODE_ENV,
-  TEST_DATABASE_URL,
-  LOCAL_DATABASE_URL,
-  DATABASE_URL
-} = require('./config');
+const { NODE_ENV, TEST_DATABASE_URL, LOCAL_DATABASE_URL, DATABASE_URL } = require('./config');
 
 module.exports = {
   migrationDirectory: 'migrations',
   driver: 'pg',
   connectionString:
-    NODE_ENV === 'test'
-      ? TEST_DATABASE_URL
-      : NODE_ENV === 'development'
-      ? LOCAL_DATABASE_URL
-      : DATABASE_URL
+    NODE_ENV === 'test' ? TEST_DATABASE_URL : NODE_ENV === 'development' ? LOCAL_DATABASE_URL : DATABASE_URL,
 };
--- services/usersService.js
+++ services/usersService.js
@@ -1,5 +1,23 @@
-const db = require('../knex/knex');
-
-const UsersService = {};
+const UsersService = {
+  insertNewUser(knex, userData) {
+    return knex
+      .insert(userData)
+      .into('users')
+      .returning('*')
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  deleteUser(knex, id) {
+    return knex('users').where({ id }).delete();
+  },
+  changeUser(knex, id, updatedUser) {
+    return knex('users')
+      .where({ id })
+      .update(updatedUser)
+      .returning('*')
+      .then((rows) => rows[0]);
+  },
+};
 
 module.exports = UsersService;
