81323c46930d0ad0a97a56e3745e5fc5017c99f2
Dominic Faulring <domcodesjs@gmail.com>
2020-12-30 14:33:04 -0800

    added real photo links

--- migrations/002.do.create_gsn_profiles.sql
+++ migrations/002.do.create_gsn_profiles.sql
@@ -3,8 +3,8 @@ CREATE TYPE hardware AS ENUM ('pc', 'ps3', 'ps4', 'ps5', 'xb360', 'xbone', 'xbse
 CREATE TABLE profiles (
     id bigserial primary key,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-    profile_url TEXT NOT NULL DEFAULT 'PLACEHOLDER FOR DEFAULT URL',
-    banner_url TEXT NOT NULL DEFAULT 'PLACEHOLDER FOR DEFAULT URL',
+    profile_url TEXT NOT NULL DEFAULT 'https://gaming-social-network.s3-us-west-2.amazonaws.com/avatar_placeholder.png',
+    banner_url TEXT NOT NULL DEFAULT 'https://gaming-social-network.s3-us-west-2.amazonaws.com/banner.jpg',
     user_location TEXT NOT NULL,
     external_usernames TEXT NOT NULL,
     preferred_hardware hardware,
