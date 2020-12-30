145e168eb5ff0d8c396f83ae9c27f08dc4b37786
Dominic Faulring <domcodesjs@gmail.com>
2020-12-30 10:12:35 -0800

    initiated siucket

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const app = require('./app');
+const server = require('http').createServer(app);
 const PORT = process.env.PORT || 5000;
+const io = require('socket.io')(server);
 
-app.listen(PORT, () => console.log(`Express is running on port ${PORT}!`));
+io.on('connection', (socket) => {
+  console.log('connected');
+});
+
+server.listen(PORT, () => console.log(`Express is running on port ${PORT}!`));
