fa362656678b8d4c9c427322a73bf80d7a92240c
Dominic Faulring <domcodesjs@gmail.com>
2021-01-04 11:30:45 -0800

    added dummy data

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { createPost, getUserPosts } = require('../controllers/postsController');
+const { v4: uuid } = require('uuid');
+// const { createPost, getUserPosts } = require('../controllers/postsController');
 
-router.get('/', passport.authenticate('jwt', { session: false }), getUserPosts);
-router.post('/', passport.authenticate('jwt', { session: false }), createPost);
+// router.get('/', passport.authenticate('jwt', { session: false }), getUserPosts);
+// router.post('/', passport.authenticate('jwt', { session: false }), createPost);
+
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      posts: [
+        {
+          entity_id: uuid(),
+          user_id: 1,
+          post_text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          entity_id: uuid(),
+          user_id: 1,
+          post_text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          entity_id: uuid(),
+          user_id: 1,
+          post_text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        }
+      ]
+    });
+  }
+);
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.status(200).json({
+      success: true,
+      post: {
+        entity_id: uuid(),
+        user_id: req.user.id,
+        post_text: req.body.text
+      }
+    });
+  }
+);
 
 module.exports = router;
