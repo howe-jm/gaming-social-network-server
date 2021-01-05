e5b5085ba10fcae16a8db68899e2a632b18e3bf8
Donovan Le <dtle647@gmail.com>
2021-01-05 13:27:49 -0600

    something

--- migrations/002.do.create_gsn_profiles.sql
+++ migrations/002.do.create_gsn_profiles.sql
@@ -8,4 +8,3 @@ CREATE TABLE profiles (
     preferred_hardware hardware,
     user_bio TEXT
 );
-INSERT INTO profiles(user_id, user_location, external_usernames, preferred_hardware, gamer_type, user_bio) VALUES (1, 'Bentonville', '@dr0wzie', 'pc', true, 'I am a hardcore Mario Cart player, I love drinking seltzer water with a hint of caramelized bacon. I have 124 pet lizards, all named after the extras in Lord of the Ring.');
