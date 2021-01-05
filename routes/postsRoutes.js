237803c3cf0e25b44131967446771edf10c64e86
Dominic Faulring <domcodesjs@gmail.com>
2021-01-05 10:40:00 -0800

    create and get a user posts works

--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,58 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { v4: uuid } = require('uuid');
-// const { createPost, getUserPosts } = require('../controllers/postsController');
+const { createPost, getUserPosts } = require('../controllers/postsController');
 
-// router.get('/', passport.authenticate('jwt', { session: false }), getUserPosts);
-// router.post('/', passport.authenticate('jwt', { session: false }), createPost);
-
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.status(200).json({
-      success: true,
-      posts: [
-        {
-          entity_id: uuid(),
-          user_id: 1,
-          username: 'darissdev',
-          post_text:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        },
-        {
-          entity_id: uuid(),
-          user_id: 1,
-          username: 'darissdev',
-          post_text:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        },
-        {
-          entity_id: uuid(),
-          user_id: 1,
-          username: 'darissdev',
-          post_text:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        }
-      ]
-    });
-  }
-);
-router.post(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.status(200).json({
-      success: true,
-      post: {
-        entity_id: uuid(),
-        user_id: req.user.id,
-        username: 'darissdev',
-        post_text: req.body.text
-      }
-    });
-  }
-);
+router.get('/', passport.authenticate('jwt', { session: false }), getUserPosts);
+router.post('/', passport.authenticate('jwt', { session: false }), createPost);
 
 module.exports = router;
--- services/postsService.js
+++ services/postsService.js
@@ -1,12 +1,17 @@
 const db = require('../knex/knex');
 
-exports.insertPost = async (content, user_id) => {
-  const post = (await db('posts').insert({ content, user_id }).returning('*'))[0];
+exports.insertPost = async (post_text, user_id) => {
+  const entity = (await db('entity').insert({}).returning('*'))[0];
+  const post = (
+    await db('entity_post')
+      .insert({ entity_id: entity.id, user_id, post_text })
+      .returning('*')
+  )[0];
   return post;
 };
 
 exports.getUserPosts = async (user_id) => {
-  const posts = await db('posts').where({ user_id }).andWhere({ group_id: null }).returning('*');
+  const posts = await db('entity_post').where({ user_id }).returning('*');
   return posts;
 };
 
