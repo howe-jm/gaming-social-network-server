71024dd6f0b4c690b3619290fa48bafd58c49318
Dominic Faulring <domcodesjs@gmail.com>
2021-01-05 12:16:02 -0800

    able to create comments

---
+++ controllers/commentsController.js
@@ -0,0 +1,26 @@
+const { insertComment } = require('../services/commentsService');
+
+exports.createComment = async (req, res) => {
+  try {
+    const { comment_text, entity_id } = req.body;
+    const user_id = req.user.id;
+    const comment = await insertComment(entity_id, user_id, comment_text);
+
+    if (!comment) {
+      return res.status(400).json({
+        success: false,
+        errors: [{ msg: 'Could not create comment' }]
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      comment
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      errors: [{ msg: 'Server error: Could not create comment' }]
+    });
+  }
+};
---
+++ services/commentsService.js
@@ -0,0 +1,15 @@
+const db = require('../knex/knex');
+
+exports.insertComment = async (entity_id, user_id, comment_text) => {
+  const comment = (
+    await db('entity_comment')
+      .insert({
+        entity_id,
+        user_id,
+        comment: comment_text
+      })
+      .returning('*')
+  )[0];
+  console.log(comment);
+  return comment;
+};
