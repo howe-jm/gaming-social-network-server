b480b160ac7f45806fa94d80aa15c1e88ea48fac
Dominic Faulring <domcodesjs@gmail.com>
2021-01-06 10:38:57 -0800

    changed something to bigint

--- migrations/008.do.create_favorite_games.sql
+++ migrations/008.do.create_favorite_games.sql
@@ -1,5 +1,5 @@
 CREATE TABLE fav_games (
     id BIGSERIAL PRIMARY KEY,
-    user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE
+    user_id BIGINT REFERENCES users(id) ON DELETE CASCADE,
     game_json TEXT NOT NULL
 );
