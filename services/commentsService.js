8c1c2a1d609e26308b2023366f6dda910312ad7c
Dominic Faulring <domcodesjs@gmail.com>
2021-01-06 08:12:48 -0800

    curr

--- services/commentsService.js
+++ services/commentsService.js
@@ -1,15 +1,20 @@
 const db = require('../knex/knex');
 
 exports.insertComment = async (entity_id, user_id, comment_text) => {
-  const comment = (
-    await db('entity_comment')
-      .insert({
-        entity_id,
-        user_id,
-        comment: comment_text
-      })
-      .returning('*')
-  )[0];
-  console.log(comment);
-  return comment;
+  try {
+    const comment = (
+      await db('entity_comment')
+        .insert({
+          entity_id,
+          user_id,
+          comment: comment_text
+        })
+        .returning('*')
+    )[0];
+    console.log(comment);
+
+    return comment;
+  } catch (err) {
+    console.log(err);
+  }
 };
