7fe8d2d5489d9796cf179b121ef34a390717b39e
Donovan Le <dtle647@gmail.com>
2021-01-06 14:01:23 -0600

    something

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -2,32 +2,31 @@ const { retrievePostComments } = require('../services/commentsService');
 const { insertPost, getUserPosts } = require('../services/postsService');
 
 exports.createPost = async (req, res) => {
-  try {
-    const { post_text } = req.body;
-    const user_id = req.user.id;
-    const post = await insertPost(post_text, user_id);
+    try {
+        const { post_text } = req.body;
+        const user_id = req.user.id;
+        const post = await insertPost(post_text, user_id);
 
-    if (!post) {
-      return res.status(400).json({
-        success: false,
-        errors: [{ msg: 'Could not create post' }]
-      });
+        if (!post) {
+            return res.status(400).json({
+                success: false,
+                errors: [{ msg: 'Could not create post' }],
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            post,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            errors: [{ msg: 'Server error: Could not create post' }],
+        });
     }
-
-    res.status(200).json({
-      success: true,
-      post
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      errors: [{ msg: 'Server error: Could not create post' }]
-    });
-  }
 };
 
 exports.getUserPosts = async (req, res) => {
-<<<<<<< HEAD
     try {
         const user_id = req.user.id;
         const posts = await getUserPosts(user_id);
@@ -44,56 +43,33 @@ exports.getUserPosts = async (req, res) => {
             posts,
         });
     } catch (err) {
-        console.log(err);
         res.status(500).json({
             success: false,
             errors: [{ msg: 'Server error: Could not get posts' }],
         });
-=======
-  try {
-    const user_id = req.user.id;
-    const posts = await getUserPosts(user_id);
-
-    if (!posts) {
-      return res.status(400).json({
-        success: false,
-        errors: [{ msg: 'Could not get posts' }]
-      });
->>>>>>> 9839bb28ba1749a2e78b040895d0f7e11b421652
     }
-
-    res.status(200).json({
-      success: true,
-      posts
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      errors: [{ msg: 'Server error: Could not get posts' }]
-    });
-  }
 };
 
 exports.getPostComments = async (req, res) => {
-  try {
-    const entity_id = req.params.entityId;
-    const comments = await retrievePostComments(entity_id);
+    try {
+        const entity_id = req.params.entityId;
+        const comments = await retrievePostComments(entity_id);
 
-    if (!comments) {
-      return res.status(400).json({
-        success: false,
-        errors: [{ msg: 'Could not get comments' }]
-      });
+        if (!comments) {
+            return res.status(400).json({
+                success: false,
+                errors: [{ msg: 'Could not get comments' }],
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            comments,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            errors: [{ msg: 'Server error: Could not get comments' }],
+        });
     }
-
-    res.status(200).json({
-      success: true,
-      comments
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      errors: [{ msg: 'Server error: Could not get comments' }]
-    });
-  }
 };
