9dc6d0cdeec30b103d25c9b7b02a6436aae0eb7f
Jason Howe <trisynth@gmail.com>
2021-01-07 14:44:11 -0500

    Groups endpoint updated for get and filtered get requests.

--- routes/groupsRoutes.js
+++ routes/groupsRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { createGroup } = require('../controllers/groupsController');
+const { createGroup, getGroups, filterGroups } = require('../controllers/groupsController');
 
 router.post('/', passport.authenticate('jwt', { session: false }), createGroup);
+
+router.get('/', passport.authenticate('jwt', { session: false }), getGroups);
+
+router.get('/filter', passport.authenticate('jwt', { session: false }), filterGroups);
 
 module.exports = router;
