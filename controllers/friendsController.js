e68b36fb8385a8428a4df4128d1ade48478b9ecf
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-11 14:43:12 -0600

    added first controller function

---
+++ controllers/friendsController.js
@@ -0,0 +1,25 @@
+const {
+    getFriendReqs, 
+    getSentReqs, 
+    getPendingReqs,
+    getCurrentFriends, 
+    removeFriend, 
+    acceptFriend 
+} = require('../services/friendsService');
+
+exports.getAllCurrentFriends = async (req, res) => {
+    try {
+        const user_b = req.user.id;
+        const allCurrentFriends = await getCurrentFriends(user_b);
+        const returnAllCurrentFriends = allCurrentFriends.map((friend) => JSON.parse(friend.user_a))
+
+        res.status(200).json({success: true, returnAllCurrentFriends});
+    } catch (err) {
+        res.status(500).json({
+            success: false, 
+            errors: [{msg: 'Server error: Could not get current friends'}]
+        });
+    }
+};
+
+// next function is to get all pending requests
--- services/friendsService.js
+++ services/friendsService.js
@@ -1,16 +1,11 @@
 const db = require('../knex/knex');
 
-<<<<<<< HEAD
 
 // user_a is the sender, user_b is the receiver
 
 
 
 // the below function returns all requests received by user_b
-=======
-//user_a is requesting, user_b is the receiving
-//function returns all requests received 
->>>>>>> df44582f7ca6ad56a90395d96e76a3cdb4c8d26b
 exports.getFriendReqs = async (user_b) => {
     const friends = await db
         .from('friends')
@@ -19,7 +14,6 @@ exports.getFriendReqs = async (user_b) => {
     return friends;
 };
 
-<<<<<<< HEAD
 
 // the below function returns all outgoing friend requests for user_a
 exports.getSentReqs = async (user_a) => {
@@ -30,20 +24,17 @@ exports.getSentReqs = async (user_a) => {
     return friends;
 }; 
 
-// the below function returns all pending friend requests for user_b
-exports.getPendingReqs = async (user_b) => {
-=======
-//function returns all outgoing friend requests
-exports.getSentReqs = async (user_a) => {
+exports.getCurrentFriends = async (user_b) => {
     const friends = await db
-      .from("friends")
-      .where("user_a", user_a)
-      .returning("*");
+        .from('friends')
+        .where('user_b', user_b)
+        .andWhere('pending', false)
+        .returning('*');
     return friends;
 };
 
-exports .getPendingReqs = async (user_b) => {
->>>>>>> df44582f7ca6ad56a90395d96e76a3cdb4c8d26b
+// the below function returns all pending friend requests for user_b
+exports.getPendingReqs = async (user_b) => {
     const pendingReqs = await db
         .from('friends')
         .where('user_b', user_b)
@@ -52,7 +43,7 @@ exports .getPendingReqs = async (user_b) => {
     return pendingReqs;
 };
 
-<<<<<<< HEAD
+
 exports.removeFriend = (user_b, user_a) => {
     return db('friends')
     .where('user_b', user_b)
@@ -61,9 +52,6 @@ exports.removeFriend = (user_b, user_a) => {
 };
 
 exports.acceptFriend = async (user_b, user_a) => {
-=======
-exports.acceptFriend = async (user_b, user_a) => 
->>>>>>> df44582f7ca6ad56a90395d96e76a3cdb4c8d26b
     const accepted = false;
     const acceptedFriend = await db
         .from('friends')
@@ -71,17 +59,6 @@ exports.acceptFriend = async (user_b, user_a) =>
         .andWhere('user_a', user_a)
         .update({pending: accepted});
     return acceptedFriend;
-<<<<<<< HEAD
 };
 
 exports.insertFriend = (user_id, username) => {};
-=======
-}
-
-exports.removeFriend = (user_b, user_a) => {
-    return db('friends')
-    .where('user_b', user_b)
-    .andWhere('user_a', user_a)
-    .delete();
-};
->>>>>>> df44582f7ca6ad56a90395d96e76a3cdb4c8d26b
