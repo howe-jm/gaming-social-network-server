c6ee6de9bc7cefac8abf1432e98f6d3191f6c625
Joshua Vaughan <jvaughan007@yahoo.com>
2021-01-11 11:29:27 -0600

    made friendsRoutes file

---
+++ routes/friendsRoutes.js
@@ -0,0 +1,12 @@
+// @GET /friends - gets all friends associated with the user
+// @GET /friends/requests - gets all pending friend requests a user has received
+// @DELETE /friends/:friendId - deletes a friend request
+// @PATCH /friends/:friendId - accepts a friend request updating `pending` to false meaning they are now friends
+
+/*
+router.get('/')
+router.get('/requests')
+router.delete('/:friendId');
+router.patch('/:friendId');
+ */
+
