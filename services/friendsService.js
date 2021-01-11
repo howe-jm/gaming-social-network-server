df44582f7ca6ad56a90395d96e76a3cdb4c8d26b
Dariss Harris <darissharris@gmail.com>
2021-01-11 10:53:01 -0800

    friendServices completed

--- services/friendsService.js
+++ services/friendsService.js
@@ -1,3 +1,47 @@
 const db = require('../knex/knex');
 
-exports.insertFriend = (user_id, username) => {};
+//user_a is requesting, user_b is the receiving
+//function returns all requests received 
+exports.getFriendReqs = async (user_b) => {
+    const friends = await db
+        .from('friends')
+        .where('user_b', user_b)
+        .returning('*');
+    return friends;
+};
+
+//function returns all outgoing friend requests
+exports.getSentReqs = async (user_a) => {
+    const friends = await db
+      .from("friends")
+      .where("user_a", user_a)
+      .returning("*");
+    return friends;
+};
+
+exports .getPendingReqs = async (user_b) => {
+    const pendingReqs = await db
+        .from('friends')
+        .where('user_b', user_b)
+        .andWhere('pending', true)
+        .returning('*');
+    return pendingReqs;
+};
+
+exports.acceptFriend = async (user_b, user_a) => 
+    const accepted = false;
+    const acceptedFriend = await db
+        .from('friends')
+        .where('user_b', user_b)
+        .andWhere('user_a', user_a)
+        .update({pending: accepted});
+    return acceptedFriend;
+}
+
+
+exports.removeFriend = (user_b, user_a) => {
+    return db('friends')
+    .where('user_b', user_b)
+    .andWhere('user_a', user_a)
+    .delete();
+};
