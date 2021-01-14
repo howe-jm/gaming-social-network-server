ab58c829aaf0c71792010fc48af2788e903d1339
Donovan Le <dtle647@gmail.com>
2021-01-14 14:23:34 -0600

    fixed user bio patch

--- routes/profilesRoutes.js
+++ routes/profilesRoutes.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const { body } = require('express-validator');
+const bodyParser = require('body-parser');
 const { login } = require('../controllers/authController');
 const uploadImage = require('../services/imagesService');
+const { updateUserProfileBio } = require('../controllers/profilesController');
 
-router.post('/profile/:username');
+router.patch('/:username', bodyParser.json(), updateUserProfileBio);
 router.patch('/update/image', uploadImage.single('image'), (req, res) => {
-  res.json({
-    success: true,
-    imageURL: req.file.location
-  });
+    res.json({
+        success: true,
+        imageURL: req.file.location,
+    });
 });
 
 module.exports = router;
--- services/profilesService.js
+++ services/profilesService.js
@@ -1,9 +1,19 @@
 const db = require('../knex/knex');
 
 exports.getUserProfile = async (id) => {
-  const user = (await db('users').where({ username }).returning('*'))[0];
-  const profile = (
-    await db('profiles').where({ user_id: user.id }).returning('*')
-  )[0];
-  return profile;
+    const user = (await db('users').where({ username }).returning('*'))[0];
+    const profile = (
+        await db('profiles').where({ user_id: user.id }).returning('*')
+    )[0];
+    return profile;
+};
+
+exports.updateUserBio = async (user_bio, user_id) => {
+    const profile = (
+        await db('profiles')
+            .update('user_bio', user_bio)
+            .where({ user_id: user_id })
+            .returning('*')
+    )[0];
+    return profile;
 };
