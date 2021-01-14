c1fc163e55562c92b024a82a31686cfb67ee0457
Dominic Faulring <domcodesjs@gmail.com>
2021-01-14 08:41:10 -0800

    removed eslint comment

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-/* eslint-disable indent */
-'use strict';
 require('dotenv').config();
 
 module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET,
-    NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
-    LOCAL_DATABASE_URL: process.env.LOCAL_DATABASE_URL,
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: process.env.NODE_ENV,
+  DATABASE_URL: process.env.DATABASE_URL,
+  LOCAL_DATABASE_URL: process.env.LOCAL_DATABASE_URL,
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
 };
