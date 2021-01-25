3bc11dd8927e628972d81cbd5311086f1fd3d380
Donovan Le <dtle647@gmail.com>
2021-01-25 10:58:31 -0600

    added requests table

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,56 +5,59 @@ const { JWT_SECRET } = require('../config');
 const { getUserByEmail } = require('../services/authService');
 
 exports.login = async (req, res) => {
-  try {
-    const errors = validationResult(req);
+    try {
+        const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(401).json({ success: false, errors: errors.array() });
+        if (!errors.isEmpty()) {
+            return res
+                .status(401)
+                .json({ success: false, errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+        console.log(email, password);
+        const user = await getUserByEmail(email);
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                errors: [{ msg: 'Invalid email or password' }],
+            });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({
+                success: false,
+                errors: [{ msg: 'Invalid email or password' }],
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                role: user.role,
+            },
+            JWT_SECRET
+        );
+
+        res.json({
+            success: true,
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                role: user.role,
+            },
+            token,
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            errors: [{ msg: 'Server error: Failed to login user' }],
+        });
     }
-
-    const { email, password } = req.body;
-    const user = await getUserByEmail(email);
-
-    if (!user) {
-      return res.status(401).json({
-        success: false,
-        errors: [{ msg: 'Invalid email or password' }]
-      });
-    }
-
-    const passwordMatch = await bcrypt.compare(password, user.password);
-
-    if (!passwordMatch) {
-      return res.status(401).json({
-        success: false,
-        errors: [{ msg: 'Invalid email or password' }]
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-        role: user.role
-      },
-      JWT_SECRET
-    );
-
-    res.json({
-      success: true,
-      user: {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-        role: user.role
-      },
-      token
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      errors: [{ msg: 'Server error: Failed to login user' }]
-    });
-  }
 };
