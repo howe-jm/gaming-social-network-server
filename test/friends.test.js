a5e2d872ed83a8bdefa2b69ad14c71248043379a
Jason Howe <trisynth@gmail.com>
2021-01-25 13:49:46 -0500

    Comments denoting intended test refactor at a later date.

--- test/friends.test.js
+++ test/friends.test.js
@@ -3,6 +3,11 @@ const { expect } = require('chai');
 const request = require('supertest');
 const { createTables, dropTables, createUser } = require('./testHelpers');
 
+/* 
+Note: Not final test code. Will refactor post-deployment. 
+These tests check the basic functionalty of the app in an inefficient manner, but they work. 
+*/
+
 describe('/friends route', () => {
   it('GET should retrieve all current friends', async () => {
     await dropTables();
--- test/users.test.js
+++ test/users.test.js
@@ -3,6 +3,11 @@ const { expect } = require('chai');
 const request = require('supertest');
 const { createTables, dropTables, createUser } = require('./testHelpers');
 
+/* 
+Note: Not final test code. Will refactor post-deployment. 
+These tests check the basic functionalty of the app in an inefficient manner, but they work. 
+*/
+
 describe('/users should post a new user', () => {
   it('/users should post a new user', async () => {
     await dropTables();
