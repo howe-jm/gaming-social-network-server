5ee0491cf686697a525cb280fcfb26428f1767ae
Dominic Faulring <domcodesjs@gmail.com>
2021-01-25 18:28:10 -0800

    current

--- controllers/groupsController.js
+++ controllers/groupsController.js
@@ -60,29 +60,16 @@ exports.getGroups = async (req, res) => {
 };
 
 exports.getGroup = async (req, res) => {
-    try {
-        const { slug } = req.params;
-        const user = req.user;
-        const group = await retrieveGroup(slug, user);
+  try {
+    const { slug } = req.params;
+    const user = req.user;
+    const group = await retrieveGroup(slug, user);
 
-        if (!group) {
-            return res.status(400).json({
-                success: false,
-                errors: [{ message: 'Group not found' }],
-            });
-        }
-
-        const isMember = await isUserInGroup(group.entity_id, group.id, user);
-        const members = await getGroupMembers(group.id);
-
-        return res
-            .status(200)
-            .json({ success: true, group, members, isMember });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            errors: [{ msg: 'Server error ' }],
-        });
+    if (!group) {
+      return res.status(400).json({
+        success: false,
+        errors: [{ message: 'Group not found' }]
+      });
     }
 
     const isMember = await isUserInGroup(group.entity_id, group.id, user);
@@ -131,26 +118,31 @@ exports.joinGroup = async (req, res) => {
         errors: [{ message: 'Could not join group' }]
       });
     }
+
+    return res.status(200).json({ success: true, member });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.leaveGroup = async (req, res) => {
-    try {
-        const user = req.user;
-        const { entity_id, group_id } = req.body;
+  try {
+    const user = req.user;
+    const { entity_id, group_id } = req.body;
 
-        const member = await removeMemberFromGroup(entity_id, group_id, user);
+    const member = await removeMemberFromGroup(entity_id, group_id, user);
 
-        if (!member) {
-            return res.status(400).json({
-                success: false,
-                errors: [{ message: 'Could not leave group' }],
-            });
-        }
-
-        return res.status(200).json({ success: true, member });
-    } catch (err) {
-        console.log(err);
+    if (!member) {
+      return res.status(400).json({
+        success: false,
+        errors: [{ message: 'Could not leave group' }]
+      });
     }
+
+    return res.status(200).json({ success: true, member });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 exports.createGroupPost = async (req, res) => {
